da9d210fa118946d843fc9de439c9810b34bdf65
Luis Henrique Medeiros <lhmedeiros0@gmail.com>
2017-05-13 13:44:20 -0300

    Primeiro trabalho iniciado

---
+++ trabalhos/disassembler.s
@@ -0,0 +1,12 @@
+# Projeto: Desenvolver um programa disassembler (desmontador ou desassemblador) para o processador MIPS.
+
+# O programa faz a leitura de um arquivo com o código em linguagem de máquina, processa e escreve o 
+# correspondente arquivo em linguagem de montagem.
+
+# A equipe (dupla) deve escrever, testar e documentar o programa. 
+
+# Enviar via Moodle o relatório (em pdf) do projeto. Anexar o código documentado do programa.
+# Enviar via Moodle o código do programa.
+
+# Em anexo temos um código em linguagem de máquina que deve ser desmontado. O código inicia no
+# endereço 0x00400000
